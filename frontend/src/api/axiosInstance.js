b972310ffad9be05a34736754199e44cba6e4db7
AryanSachan12 <aryansachan2004@gmail.com>
2025-05-04 23:55:42 +0530

    Changed axios config

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api', // Set base URL for all requests
+  baseURL: 'https://jobpredict.onrender.com/', // Set base URL for all requests
   headers: {
     'Content-Type': 'application/json',
   },
