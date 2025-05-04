595f9f79b69042dfac6aaecd679e73aa8b2528ce
AryanSachan12 <aryansachan2004@gmail.com>
2025-05-04 23:51:29 +0530

    Setup deployment

--- fastapi/database.py
+++ fastapi/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = "mysql+pymysql://root:admin@localhost:3306/dbmsproject"
+URL_DATABASE = "sqlite:///./dbms.db"
 
 engine = create_engine(URL_DATABASE)
 
--- fastapi/main.py
+++ fastapi/main.py
@@ -4,6 +4,15 @@ import models
 from database import engine
 from routes.user_routes import router as user_router
 from routes.machine_learning_routes import router as ml_router
+from dotenv import load_dotenv
+import uvicorn
+import os
+
+load_dotenv()
+
+port = int(os.environ.get("PORT"))
+
+print(port)
 
 app = FastAPI(
     title="basic-fastapi",
@@ -25,3 +34,6 @@ models.Base.metadata.create_all(bind=engine)
 # Include the user router
 app.include_router(user_router)
 app.include_router(ml_router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=port)
